f42010a146795111181d8ee8f727c571f947ddbe
Ben Marwick <benmarwick@hotmail.com>
2020-07-06 13:17:58 -0700

    seperate script file for similarity and distance analysis

--- analysis/code/word-stats-plots.R
+++ analysis/code/word-stats-plots.R
@@ -32,6 +32,8 @@ dfm_keywords <-
              pattern = keywords,
              selection = "keep")
 
+saveRDS(dfm_keywords, here::here("analysis","data","dfm_keywords.rds"))
+
 dfm_keywords_tbl <-
   convert(dfm_keywords, to = "data.frame") %>%
   pivot_longer(-document,
@@ -86,33 +88,8 @@ ggplot(data = dfm_keywords_tbl_prop,
   theme(axis.text.x = element_text(angle = 90,
                                    vjust = 0.5))
 
-# distances for specific documents for keyword
-dist_keywords <- textstat_dist(dfm_keywords,
-                               dfm_keywords[,"discrimination"],
-                               margin = "documents")
 
-dist_keywords_tbl <-
-  as.data.frame(dist_keywords, to = "data.frame")
-
-# compute similarities between features using relative frequency
-simi_all <-
-  dfm_weight(all_text_c_dtm, scheme = "prop") %>%
-  textstat_simil(selection = keywords,
-                 method = "correlation",
-                 margin = "features")
-
-head(as.matrix(simi_all), 10)
-as.list(simi_all, n = 10)
-
-# compute similarities between features
-simi_keywords <- textstat_simil(dfm_keywords,
-                                all_text_c_dtm[, c("black",
-                                                   "people",
-                                                   "african",
-                                                   "asian")],
-                                method = "correlation",
-                                margin = "features")
-head(as.matrix(simi_keywords), 10)
+#-------------------------------------------------------
 
 # read data from googlesheet
 library(googlesheets4)
