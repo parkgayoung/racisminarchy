cc693abdc4f7cb18537eabff611b93b5a2c7ba06
Ben Marwick <benmarwick@hotmail.com>
2020-09-01 14:56:56 -0700

    stash

--- analysis/code/003-kwik.R
+++ analysis/code/003-kwik.R
@@ -1,4 +1,5 @@
 library(quanteda)
+library(tidyverse)
 
 # https://quanteda.io/reference/textstat_collocations.html
 
@@ -15,6 +16,21 @@ if(!exists("all_text_c")){
 if(!exists("toks")){
   toks <- tokens(all_text_c)
 }
+
+# what are the tfidf values overall,
+# so we can compare to the values in the kwik
+toks_dfm <-
+toks %>%
+  dfm(remove = stopwords(),
+      verbose = TRUE,
+      remove_punct = TRUE) %>%
+  dfm_tfidf(
+    scheme_tf = "count",
+    scheme_df = "inverse",
+    force = FALSE
+  )
+
+
 
 
 #character vector: separating words by whitespaces and wrap the vector by phrase()
@@ -42,9 +58,11 @@ kwic_output %>%
 
 # prepare to plot
 
-# choose a number
-
+# store output
 kwic_plots <- vector("list", length = length(multiword))
+pre_post_tfidf <- vector("list", length = length(multiword))
+x_pre <- vector("list", length = length(multiword))
+x_post <- vector("list", length = length(multiword))
 
 for(i in 1:length(multiword)){
 keyword <- i
@@ -54,7 +72,7 @@ keyword_n <- kwic_output_tidy$keyword[keyword]
 #----------------------------------------------
 # tf-idf weighting
 
-x_pre <-
+x_pre[[i]] <-
 kwic_output_tidy$data[[keyword]]$pre %>%
   dfm(remove = stopwords(),
       verbose = TRUE,
@@ -66,8 +84,8 @@ dfm_tfidf(
 )
 
 x_tbl_pre <-
-  tibble(featnames = names(x_pre@x),
-         tfidf = x_pre@x) %>%
+  tibble(featnames = names(x_pre[[i]]@x),
+         tfidf = x_pre[[i]]@x) %>%
   group_by(featnames) %>%
   summarise(sum_tfidf = sum(tfidf, na.rm = TRUE)) %>%
   arrange(desc(sum_tfidf)) %>%
@@ -75,7 +93,7 @@ x_tbl_pre <-
   rownames_to_column()
 
 
-x_post <-
+x_post[[i]] <-
   kwic_output_tidy$data[[keyword]]$post %>%
   dfm(remove = stopwords(),
       verbose = TRUE,
@@ -87,15 +105,15 @@ x_post <-
   )
 
 x_tbl_post <-
-  tibble(featnames = names(x_post@x),
-         tfidf = x_post@x) %>%
+  tibble(featnames = names(x_post[[i]]@x),
+         tfidf = x_post[[i]]@x) %>%
   group_by(featnames) %>%
   summarise(sum_tfidf = sum(tfidf, na.rm = TRUE)) %>%
   arrange(desc(sum_tfidf)) %>%
   slice(1:15) %>%
   rownames_to_column()
 
-pre_post_tfidf <- bind_rows(pre = x_tbl_pre,
+pre_post_tfidf[[i]] <- bind_rows(pre = x_tbl_pre,
                             post = x_tbl_post,
                             .id = "pos") %>%
   mutate(rank = parse_number(rowname)) %>%
@@ -103,13 +121,13 @@ pre_post_tfidf <- bind_rows(pre = x_tbl_pre,
 
 kwic_plots[[i]] <-
 ggplot() +
-  geom_text(data = pre_post_tfidf %>% filter(pos == "pre"),
+  geom_text(data = pre_post_tfidf[[i]] %>% filter(pos == "pre"),
             aes(x  = 2,
                 y = rank,
                 label = featnames,
                 size  = sum_tfidf)
             ) +
-  geom_text(data = pre_post_tfidf %>% filter(pos == "post"),
+  geom_text(data = pre_post_tfidf[[i]] %>% filter(pos == "post"),
             aes(x  = 8,
                 y = rank,
                 label = featnames,
@@ -146,5 +164,29 @@ plot_grid(plotlist = kwic_plots, nrow = 2)
 ggsave(here::here("analysis/figures/003-keyword-kwik.png"),
        h = 7,
        w = 17)
+
+# check distributions of tfidf
+# histogram of all tfidf values
+keywords_tfidf_hist_plot <-
+map(pre_post_tfidf, ~.x %>% select(sum_tfidf) %>% pull) %>% unlist %>%
+tibble(x = .) %>%
+  ggplot() +
+  aes(x) +
+  geom_histogram() +
+  scale_x_log10()
+pre_post_tfidf
+
+# histogram of all tfidf values
+allwords_tfidf_hist_plot <-
+tibble(x = as.vector(toks_dfm)) %>%
+  ggplot() +
+  aes(x) +
+  geom_histogram() +
+  scale_x_log10()
+
+plot_grid(keywords_tfidf_hist_plot,
+          allwords_tfidf_hist_plot,
+          nrow = 2)
+
 
 #-----------------------------------------------------
