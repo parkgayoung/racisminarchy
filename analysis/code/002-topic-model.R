8ecee39e3d1d0df4b2a1ce0d009425f0359c1f84
Gayoung Park <gayoungp@uw.edu>
2020-12-17 14:40:29 -0800

    Set seed value to get same result everytime we run the code

--- analysis/code/002-topic-model.R
+++ analysis/code/002-topic-model.R
@@ -64,10 +64,13 @@ rm(all_text_c_dtm)
 rm(all_text_c)
 rm(all_text)
 
+# set seed to get same result for every run
+set.seed(100)
 saaFit <- stm(all_text_c_dtm_trim,
               K = 0,
               max.em.its = 50,
-              init.type = "Spectral")
+              init.type = "Spectral",
+              seed = 100)
 
 # Find topics that contain user specified words.
 sl <- sageLabels(saaFit, n = 3000)
