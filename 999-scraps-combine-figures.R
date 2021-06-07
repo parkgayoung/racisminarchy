f1823c56710483e7eef4fcc349c42585cc72af2d
Gayoung Park <gayoungp@uw.edu>
2021-06-07 14:27:01 -0700

    Trying to combine same kinds of images

---
+++ 999-scraps-combine-figures.R
@@ -0,0 +1,7 @@
+library(png)
+
+#combine figures from both SAA and HA
+
+saa_simil<- readPNG(system.file("~Desktop/racisminarchy/analysis/figures/003-keyword-similar-words.png", "Rlogo.png", package="png"))
+
+ha_simil <- readPNG(system.file("003-ha-keyword-similar-words.png", "Rlogo.png", package="png"))
